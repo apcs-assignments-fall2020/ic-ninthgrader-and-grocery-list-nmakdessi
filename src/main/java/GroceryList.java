2f414311812011d49b8db4b05a64b376fb1a2c17
nmakdessi <21nmakdessi@athenian.org>
2020-11-03 17:40:25 +0000

    Completed remove() and toString()

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,34 @@
 public class GroceryList {
-    
-}
+    private String[] arr;
+
+    public GroceryList() {
+        // our grocery list will have a max size of 10
+        this.arr = new String[10]; 
+    }
+
+    public void add(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
+
+    public void remove(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                arr[i] = null;
+                break;
+            }
+        }
+    }
+
+    public String toString() {
+        String str = "Grocery List : ";
+        for (String item: arr) {
+            str += (item + "");
+        }
+        return str;
+    }
+}   
